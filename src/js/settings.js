f0bab49fa357d2b00dce1239648f8f2c2a46480a
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2025-12-12 20:20:04 +0100

    add html and style

---
+++ src/js/settings.js
@@ -0,0 +1,26 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    home: '#home',
+    products: '#products',
+    contact: '#contact',
+  },
+  nav: {
+    links: '[data-nav]',
+  },
+  products: {
+    list: '#products-list',
+  }
+};
+
+export const classNames = {
+  pages: {
+    active: 'active',
+  }
+};
+
+export const settings = {
+  api: {
+    productsUrl: 'http://localhost:3000'
+  }
+};
